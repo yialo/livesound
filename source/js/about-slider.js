7cc57d01f44f155fa154418a417562c2c57d4757
Aleksei Arro <alexey.arro@waytostart.ru>
2018-11-23 20:13:42 +0300

    About slider mobile version in progress

--- source/js/about-slider.js
+++ source/js/about-slider.js
@@ -1,7 +1,6 @@
 var historySlider = document.querySelector('.history__inner');
 var sliderButtonPrevious = historySlider.querySelector('.history__control-button--previous');
 var sliderButtonNext = historySlider.querySelector('.history__control-button--next');
-
 var historyDateButton_1 = historySlider.querySelector('.history__date-button--1');
 var historyDateButton_2 = historySlider.querySelector('.history__date-button--2');
 var historyDateButton_3 = historySlider.querySelector('.history__date-button--3');
@@ -37,13 +36,7 @@ sliderButtonPrevious.addEventListener('click', function() {
 });
 
 sliderButtonNext.addEventListener('click', function() {
-  if (historySlider.classList.contains('js-slide-5')) {
-    historySlider.classList.add('js-slide-1');
-    historySlider.classList.remove('js-slide-2');
-    historySlider.classList.remove('js-slide-3');
-    historySlider.classList.remove('js-slide-4');
-    historySlider.classList.remove('js-slide-5');
-  } else if (historySlider.classList.contains('js-slide-4')) {
+  if (historySlider.classList.contains('js-slide-4')) {
     historySlider.classList.remove('js-slide-1');
     historySlider.classList.remove('js-slide-2');
     historySlider.classList.remove('js-slide-3');
