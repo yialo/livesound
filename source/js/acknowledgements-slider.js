b970ebb0b72648a0d784b9dbb1cfd92a6418e7e0
Aleksei Arro <alexey.arro@waytostart.ru>
2018-12-04 16:34:58 +0300

    Small viewport slider added

--- source/js/acknowledgements-slider.js
+++ source/js/acknowledgements-slider.js
@@ -1,8 +1,53 @@
 var
-  acknowledgementsControLPrevious = document.querySelector('.acknowledgements__slider-control--previous'),
-  acknowledgementsControLNext = document.querySelector('.acknowledgements__slider-control--next'),acknowledgementsSlider = document.querySelector('.acknowledgements__list');
+  acknowledgementsControlPrevious = document.querySelector('.acknowledgements__slider-control--previous'),
+  acknowledgementsControlNext = document.querySelector('.acknowledgements__slider-control--next'),acknowledgementsSlider = document.querySelector('.acknowledgements__list');
 
-acknowledgementsControLNext.addEventListener('click', function() {
-  acknowledgementsSlider.classList.remove('js-slide-1');
-  acknowledgementsSlider.classList.add('js-slide-2');
+acknowledgementsControlNext.addEventListener('click', function() {
+  if (acknowledgementsSlider.classList.contains('js-slide-1')) {
+    acknowledgementsSlider.classList.remove('js-slide-1');
+    acknowledgementsSlider.classList.add('js-slide-2');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-2')) {
+    acknowledgementsSlider.classList.remove('js-slide-2');
+    acknowledgementsSlider.classList.add('js-slide-3');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-3')) {
+    acknowledgementsSlider.classList.remove('js-slide-3');
+    acknowledgementsSlider.classList.add('js-slide-4');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-4')) {
+    acknowledgementsSlider.classList.remove('js-slide-4');
+    acknowledgementsSlider.classList.add('js-slide-5');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-5')) {
+    acknowledgementsSlider.classList.remove('js-slide-5');
+    acknowledgementsSlider.classList.add('js-slide-6');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-6')) {
+    acknowledgementsSlider.classList.remove('js-slide-6');
+    acknowledgementsSlider.classList.add('js-slide-7');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-7')) {
+    acknowledgementsSlider.classList.remove('js-slide-7');
+    acknowledgementsSlider.classList.add('js-slide-1');
+  }
+});
+
+acknowledgementsControlPrevious.addEventListener('click', function() {
+  if (acknowledgementsSlider.classList.contains('js-slide-1')) {
+    acknowledgementsSlider.classList.remove('js-slide-1');
+    acknowledgementsSlider.classList.add('js-slide-7');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-2')) {
+    acknowledgementsSlider.classList.remove('js-slide-2');
+    acknowledgementsSlider.classList.add('js-slide-1');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-3')) {
+    acknowledgementsSlider.classList.remove('js-slide-3');
+    acknowledgementsSlider.classList.add('js-slide-2');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-4')) {
+    acknowledgementsSlider.classList.remove('js-slide-4');
+    acknowledgementsSlider.classList.add('js-slide-3');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-5')) {
+    acknowledgementsSlider.classList.remove('js-slide-5');
+    acknowledgementsSlider.classList.add('js-slide-4');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-6')) {
+    acknowledgementsSlider.classList.remove('js-slide-6');
+    acknowledgementsSlider.classList.add('js-slide-5');
+  } else if (acknowledgementsSlider.classList.contains('js-slide-7')) {
+    acknowledgementsSlider.classList.remove('js-slide-7');
+    acknowledgementsSlider.classList.add('js-slide-6');
+  }
 });
