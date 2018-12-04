1eebd477e6f315c6a769fe32229b7a4107cfecc1
Aleksei Arro <alexey.arro@waytostart.ru>
2018-12-04 14:23:57 +0300

    Slide js programming started

--- source/js/acknowledgements-slider.js
+++ source/js/acknowledgements-slider.js
@@ -1,1 +1,8 @@
+var
+  acknowledgementsControLPrevious = document.querySelector('.acknowledgements__slider-control--previous'),
+  acknowledgementsControLNext = document.querySelector('.acknowledgements__slider-control--next'),acknowledgementsSlider = document.querySelector('.acknowledgements__list');
 
+acknowledgementsControLNext.addEventListener('click', function() {
+  acknowledgementsSlider.classList.remove('js-slide-1');
+  acknowledgementsSlider.classList.add('js-slide-2');
+});
