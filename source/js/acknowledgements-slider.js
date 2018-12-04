952ebb584961832c5eaa012437c05b5a887b1e3b
Aleksei Arro <alexey.arro@waytostart.ru>
2018-12-04 12:36:14 +0300

    Initial work started on acknowledge-slider: jQuery deleted, necessary files added

--- source/js/acknowledgements-slider.js
+++ source/js/acknowledgements-slider.js
@@ -1,18 +1,1 @@
-$(document).ready(function() {
-  $('.acknowledgements__slider').slick({
-    accessibility: true,
-    centerMode: true,
-    mobileFirst: true,
-    prevArrow: $('.acknowledgements__slider-control--previous'),
-    nextArrow: $('.acknowledgements__slider-control--next'),
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        centerMode: false
-      }
-    }]
-  });
-});
+
