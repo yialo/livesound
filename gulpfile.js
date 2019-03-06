369ad24fcc2525f8dc968ec5092cd9b72d465eca
Aleksei Arro <alexey.arro@waytostart.ru>
2019-03-06 18:58:27 +0300

    Project structure changed: images added to separate folders

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@
 var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 var del = require('del');
+var flatten = require('gulp-flatten');
 var gulp = require('gulp');
 var mincss = require('gulp-csso');
 var minimage = require('gulp-imagemin');
@@ -28,72 +29,63 @@ var minsvg = function () {
     .pipe(minimage([
       minimage.svgo({
         plugins: [
-          {cleanupAttrs: false},
-          {inlineStyles: false},
-          {removeDoctype: true},
-          {removeXMLProcInst: true},
-          {removeComments: true},
-          {removeMetadata: true},
-          {removeTitle: true},
-          {removeDesc: true},
-          {removeUselessDefs: false},
-          {removeXMLNS: false},
-          {removeEditorsNSData: true},
-          {removeEmptyAttrs: true},
-          {removeHiddenElems: true},
-          {removeEmptyText: true},
-          {removeEmptyContainers: true},
-          {removeViewBox: false},
-          {cleanupEnableBackground: true},
-          {minifyStyles: false},
-          {convertStyleToAttrs: false},
-          {convertColors: true},
-          {convertPathData: true},
-          {convertTransform: true},
-          {removeUnknownsAndDefaults: true},
-          {removeNonInheritableGroupAttrs: true},
-          {removeUselessStrokeAndFill: true},
-          {removeUnusedNS: true},
-          {cleanupIDs: false},
-          {cleanupNumericValues: true},
-          {cleanupListOfValues: true},
-          {moveElemsAttrsToGroup: true},
-          {moveGroupAttrsToElems: false},
-          {collapseGroups: true},
-          {removeRasterImages: false},
-          {mergePaths: true},
-          {convertShapeToPath: false},
-          {sortAttrs: false},
-          {removeDimensions: true},
-          {removeAttrs: true},
-          {removeElementsByAttr: false},
-          {addClassesToSVGElement: false},
-          {addAttributesToSVGElement: false},
-          {removeStyleElement: true},
-          {removeScriptElement: true}
+          { addAttributesToSVGElement: false },
+          { addClassesToSVGElement: false },
+          { cleanupAttrs: false },
+          { cleanupEnableBackground: true },
+          { cleanupIDs: false },
+          { cleanupListOfValues: true },
+          { cleanupNumericValues: true },
+          { collapseGroups: true },
+          { convertColors: true },
+          { convertPathData: true },
+          { convertShapeToPath: false },
+          { convertStyleToAttrs: false },
+          { convertTransform: true },
+          { inlineStyles: false },
+          { mergePaths: true },
+          { minifyStyles: false },
+          { moveElemsAttrsToGroup: true },
+          { moveGroupAttrsToElems: false },
+          { removeAttrs: true },
+          { removeComments: true },
+          { removeDesc: true },
+          { removeDimensions: true },
+          { removeDoctype: true },
+          { removeEditorsNSData: true },
+          { removeElementsByAttr: false },
+          { removeEmptyAttrs: true },
+          { removeEmptyContainers: true },
+          { removeEmptyText: true },
+          { removeHiddenElems: true },
+          { removeMetadata: true },
+          { removeNonInheritableGroupAttrs: true },
+          { removeRasterImages: false },
+          { removeScriptElement: true },
+          { removeStyleElement: true },
+          { removeTitle: true },
+          { removeUnknownsAndDefaults: true },
+          { removeUnusedNS: true },
+          { removeUselessDefs: false },
+          { removeUselessStrokeAndFill: true },
+          { removeViewBox: false },
+          { removeXMLNS: false },
+          { removeXMLProcInst: true },
+          { sortAttrs: false },
       ]})
     ]))
-    .pipe(gulp.dest('./source/img/'));
+    .pipe(gulp.dest('./source/img/imgout/'));
 }
 
 var minbitmap = function () {
   return gulp.src('./source/imgraw/*.{jpg,png}')
     .pipe(minimage([
-      pngquant({
-        speed: 1,
-        quality: [0.8, 0.8]
-      }),
-      zopfli({
-        more: true
-      }),
-      minimage.jpegtran({
-        progressive: true
-      }),
-      mozjpeg({
-        quality: 90
-      })
+      pngquant({ speed: 1, quality: [0.8, 0.8] }),
+      zopfli({ more: true }),
+      minimage.jpegtran({ progressive: true }),
+      mozjpeg({ quality: 90 }),
     ]))
-    .pipe(gulp.dest('./source/img/'));
+    .pipe(gulp.dest('./source/imgout/'));
 }
 
 var cleanbuild = function () {
@@ -106,12 +98,13 @@ var copyfonts = function () {
 }
 
 var copysvg = function () {
-  return gulp.src('./source/img/*.svg')
+  return gulp.src('./source/svg/*.svg')
     .pipe(gulp.dest('./build/img/'));
 }
 
 var copybitmap = function () {
-  return gulp.src('./source/img/*.{jpg,png}')
+  return gulp.src('./source/bitmaps/**/*.{jpg,png}')
+    .pipe(flatten())
     .pipe(gulp.dest('./build/img/'));
 }
 
@@ -131,9 +124,7 @@ var style = function () {
     .pipe(plumber())
     .pipe(sassglob())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest('./build/css/'))
     .pipe(mincss())
     .pipe(rename('main.min.css'))
@@ -169,4 +160,3 @@ gulp.task('serve', serve);
 
 gulp.task('imagemin', gulp.parallel(minsvg, minbitmap));
 gulp.task('imagecopy', gulp.parallel(copysvg, copybitmap));
-gulp.task('imagerenew', gulp.series(gulp.parallel(minsvg, minbitmap), gulp.parallel(copysvg, copybitmap)));
