e64c42531499d7e0453c4a4e65c0453aeb13073c
Aleksei Arro <alexey.arro@waytostart.ru>
2019-03-27 17:08:54 +0300

    about block: video embedded from youtube

--- app/scripts/index-section-fadeinup.js
+++ app/scripts/index-section-fadeinup.js
@@ -3,8 +3,7 @@
 const pageMain = document.querySelector('.page-main');
 const advantagesBlock = pageMain.querySelector('.page-main__advantages');
 const directionsBlock = pageMain.querySelector('.page-main__directions');
-const firstPartnersBlock = pageMain.querySelector('.page-main__partners--first');
-const lastPartnersBlock = pageMain.querySelector('.page-main__partners--last');
+const partnersBlocks = pageMain.querySelectorAll('.page-main__partners');
 const aboutBlock = pageMain.querySelector('.page-main__about');
 
 const checkOffsetTop = function checkOffsetTopOfBlock(section) {
@@ -20,7 +19,8 @@ const checkOffsetTop = function checkOffsetTopOfBlock(section) {
 window.addEventListener('scroll', () => {
   checkOffsetTop(advantagesBlock);
   checkOffsetTop(directionsBlock);
-  checkOffsetTop(firstPartnersBlock);
-  checkOffsetTop(lastPartnersBlock);
+  partnersBlocks.forEach((block) => {
+    checkOffsetTop(block);
+  });
   checkOffsetTop(aboutBlock);
 });
