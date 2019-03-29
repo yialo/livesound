7ca2515937ee1afc8ea772a21f8dd13101a8fa9b
Aleksei Arro <alexey.arro@waytostart.ru>
2019-03-29 17:24:22 +0300

    history years-slider customization in progress

--- app/vendors_customized/cascade-slider_customized.js
+++ app/vendors_customized/cascade-slider_customized.js
@@ -5,11 +5,11 @@
     var $this = this,
       itemClass = opt.itemClass || 'cascade-slider__item',
       arrowClass = opt.arrowClass || 'cascade-slider__arrow',
-      $item = $this.find('.' + itemClass),
-      $arrow = $this.find('.' + arrowClass),
+      $item = $this.find(`.${itemClass}`),
+      $arrow = $this.find(`.${arrowClass}`),
       itemCount = $item.length;
 
-    var defaultIndex = 0;
+    var defaultIndex = 2;
 
     changeIndex(defaultIndex);
 
@@ -17,26 +17,26 @@
       var action = $(this).data('action'),
         nowIndex = $item.index($this.find('.now'));
 
-      if(action == 'next') {
-        if(nowIndex == itemCount - 1) {
+      if(action === 'next') {
+        if(nowIndex === itemCount - 1) {
           changeIndex(0);
         } else {
           changeIndex(nowIndex + 1);
         }
-      } else if (action == 'prev') {
-        if(nowIndex == 0) {
+      } else if (action === 'prev') {
+        if(nowIndex === 0) {
           changeIndex(itemCount - 1);
         } else {
           changeIndex(nowIndex - 1);
         }
       }
 
-      $('.cascade-slider__dot').removeClass('cur');
-      //$('.cascade-slider__dot').next().addClass('cur');
+      // $('.cascade-slider__dot').removeClass('cur');
+      // $('.cascade-slider__dot').next().addClass('cur');
     });
 
     // add data attributes
-    for (var i = 0; i < itemCount; i++) {
+    for (let i = 0; i < itemCount; i += 1) {
       $('.cascade-slider__item').each(function(i) {
         $(this).attr('data-slide-number', [i]);
       });
@@ -56,35 +56,35 @@
       slide.addClass('now');
       slide.next().addClass('next');
 
-      if(slide.next().length == 0) {
+      if(slide.next().length === 0) {
         $('.cascade-slider__item:first-child').addClass('next');
       }
 
-      if(slide.prev().length == 0) {
+      if(slide.prev().length === 0) {
         $('.cascade-slider__item:last-child').addClass('prev');
       }
     });
 
     function changeIndex(nowIndex) {
-      // clern all class
+      // clean all classes
       $this.find('.now').removeClass('now');
       $this.find('.next').removeClass('next');
       $this.find('.prev').removeClass('prev');
-      if(nowIndex == itemCount -1){
+      if(nowIndex === itemCount - 1){
         $item.eq(0).addClass('next');
       }
-      if(nowIndex == 0) {
-        $item.eq(itemCount -1).addClass('prev');
+      if(nowIndex === 0) {
+        $item.eq(itemCount - 1).addClass('prev');
       }
 
       $item.each(function(index) {
-        if(index == nowIndex) {
+        if(index === nowIndex) {
           $item.eq(index).addClass('now');
         }
-        if(index == nowIndex + 1 ) {
+        if(index === nowIndex + 1 ) {
           $item.eq(index).addClass('next');
         }
-        if(index == nowIndex - 1 ) {
+        if(index === nowIndex - 1 ) {
           $item.eq(index).addClass('prev');
         }
       });
