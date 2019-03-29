594fdc249be069dcbe355c6f23ea907690c605ed
Aleksei Arro <alexey.arro@waytostart.ru>
2019-03-29 18:59:37 +0300

    history slider functions extended

--- app/vendors_customized/cascade-slider_customized.js
+++ app/vendors_customized/cascade-slider_customized.js
@@ -1,30 +1,30 @@
 /* eslint-disable */
 
 (function($) {
-  $.fn.cascadeSlider = function(opt) {
-    var $this = this,
-      itemClass = 'cascade-slider__item',
-      arrowClass = 'cascade-slider__arrow',
-      $item = $this.find(`.${itemClass}`),
-      $arrow = $this.find(`.${arrowClass}`),
-      itemCount = $item.length;
+  $.fn.cascadeSlider = function (opt) {
+    const $this = this;
+    const itemClass = 'cascade-slider__item';
+    const arrowClass = 'cascade-slider__arrow';
+    const $items = $this.find(`.${itemClass}`);
+    const $arrows = $this.find(`.${arrowClass}`);
+    const itemCount = $items.length;
 
-    var defaultIndex = 2;
+    const defaultIndex = 2;
 
     changeIndex(defaultIndex);
 
-    $arrow.on('click', function() {
-      var action = $(this).data('action'),
-        nowIndex = $item.index($this.find('.now'));
+    $arrows.on('click', function () {
+      const action = $(this).data('action');
+      const nowIndex = $items.index($this.find('.now'));
 
-      if(action === 'next') {
-        if(nowIndex === itemCount - 1) {
+      if (action === 'next') {
+        if (nowIndex === itemCount - 1) {
           changeIndex(0);
         } else {
           changeIndex(nowIndex + 1);
         }
       } else if (action === 'prev') {
-        if(nowIndex === 0) {
+        if (nowIndex === 0) {
           changeIndex(itemCount - 1);
         } else {
           changeIndex(nowIndex - 1);
@@ -34,32 +34,51 @@
 
     // add data attributes
     for (let i = 0; i < itemCount; i += 1) {
-      $('.cascade-slider__item').each(function(i) {
-        $(this).attr('data-slide-number', [i]);
+      $('.cascade-slider__item').each(function (i) {
+        $(this).attr('data-slide-number', i);
       });
     }
 
     function changeIndex(nowIndex) {
       // clean all classes
-      $this.find('.now').removeClass('now');
-      $this.find('.next').removeClass('next');
+      $this.find('.prev-prev').removeClass('prev-prev');
       $this.find('.prev').removeClass('prev');
-      if(nowIndex === itemCount - 1){
-        $item.eq(0).addClass('next');
+      $this.find('.next-next').removeClass('next-next');
+      $this.find('.next').removeClass('next');
+      $this.find('.now').removeClass('now');
+
+      if (nowIndex === 0) {
+        $items.eq(itemCount - 1).addClass('prev');
+        $items.eq(itemCount - 2).addClass('prev-prev');
       }
-      if(nowIndex === 0) {
-        $item.eq(itemCount - 1).addClass('prev');
+      if (nowIndex === 1) {
+        $items.eq(0).addClass('prev');
+        $items.eq(itemCount - 1).addClass('prev-prev');
+      }
+      if (nowIndex === itemCount - 2){
+        $items.eq(0).addClass('next-next');
+        $items.eq(itemCount - 1).addClass('next');
+      }
+      if (nowIndex === itemCount - 1){
+        $items.eq(0).addClass('next');
+        $items.eq(1).addClass('next-next');
       }
 
-      $item.each(function(index) {
-        if(index === nowIndex) {
-          $item.eq(index).addClass('now');
+      $items.each(function (index) {
+        if (index === nowIndex) {
+          $items.eq(index).addClass('now');
         }
-        if(index === nowIndex + 1 ) {
-          $item.eq(index).addClass('next');
+        if (index === nowIndex - 2) {
+          $items.eq(index).addClass('prev-prev');
         }
-        if(index === nowIndex - 1 ) {
-          $item.eq(index).addClass('prev');
+        if (index === nowIndex - 1) {
+          $items.eq(index).addClass('prev');
+        }
+        if (index === nowIndex + 2) {
+          $items.eq(index).addClass('next-next');
+        }
+        if (index === nowIndex + 1) {
+          $items.eq(index).addClass('next');
         }
       });
     }
