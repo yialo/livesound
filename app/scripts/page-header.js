c152d5cf65e5bb3f2ced15a7fe079c0f0f3e44cc
Aleksei Arro <alexey.arro@waytostart.ru>
2019-04-15 19:32:08 +0300

    page-header scroll-scaling in progress

---
+++ app/scripts/page-header.js
@@ -0,0 +1,17 @@
+// 'use strict';
+
+// const pageHeader = document.querySelector('.page-header');
+
+// const checkHeaderTopOffset = function checkOffsetTopOfBlock() {
+//   const scrollShift = window.innerHeight;
+//   const scrolledTop = pageHeader.scrollTop;
+//   if (scrolledTop >= section.offsetTop - scrollShift) {
+//     section.classList.add('animatecss-fadeinup');
+//   } else {
+//     section.classList.remove('animatecss-fadeinup');
+//   }
+// };
+
+// if (document.offsetWidth >= 768) {
+
+// }
